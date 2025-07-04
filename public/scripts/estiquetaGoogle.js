a13fe8779bc12405b9ec8d102b4eefa2f797010b
lucasalonia <salonialuca97@gmail.com>
2025-07-04 12:16:50 -0300

    Incercion de tag de Google mediante script

---
+++ public/scripts/estiquetaGoogle.js
@@ -0,0 +1,12 @@
+(function() {
+  const scriptTag = document.createElement('script');
+  scriptTag.async = true;
+  scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-WPZQYV2H0B';
+  document.head.appendChild(scriptTag);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-WPZQYV2H0B');
+})();
