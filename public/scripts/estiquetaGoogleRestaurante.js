a39bb294bbea087f9412d795fd8b49c9a0026991
lucasalonia <salonialuca97@gmail.com>
2025-08-11 17:03:58 -0300

    Configuracion de etiqueta

--- public/scripts/estiquetaGoogleRestaurante.js
+++ public/scripts/estiquetaGoogleRestaurante.js
@@ -1,12 +1,15 @@
 (function() {
   const scriptTag = document.createElement('script');
   scriptTag.async = true;
-  scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=AW-312024311';
+  scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-WPZQYV2H0B';
   document.head.appendChild(scriptTag);
 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   window.gtag = gtag;
   gtag('js', new Date());
-  gtag('config', 'AW-312024311');
+
+  // Configuración de los dos IDs
+  gtag('config', 'G-WPZQYV2H0B'); // Ej: Analytics
+  gtag('config', 'AW-312024311');  // Ej: Ads
 })();
